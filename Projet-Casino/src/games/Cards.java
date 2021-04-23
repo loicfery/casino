d2b74dc8e3323e44250d062e0ef6eca854e8871d
MaxLGD89 <maximelegal89100@gmail.com>
2021-04-23 15:01:39 +0200

    Premier aspect de la Classe Cards_Package

--- Projet-Casino/src/games/Cards.java
+++ Projet-Casino/src/games/Cards.java
@@ -4,25 +4,14 @@ import java.util.ArrayList;
 
 public class Cards {
 
-    private String sign; // essayer de faire un enum pour {HEART, CLOVER, SPADES, TILE}
-    private int number; // 1 à 10 normale, 11 J / 12 Q / 13 K
+    private String sign;
+    private int number; // 0 à 9 = 1 = 10, 10 = J, 11 = Q, 12 = K
     private ArrayList<Cards> card_package; // Type provisoire, à adapter au besoin
 
-    public Cards(String sign, int number) { // A modifier quand l'enum sera fait
-        this.sign = sign;
+    public Cards(int number, String sign) { // A modifier quand l'enum sera fait
         this.number = number;
-    }
+        this.sign = sign;
 
-    public boolean empty_package(){ //Vérifie si le paquet est vide
-        return true;
-    }
-
-    public void sorted_package(ArrayList<Cards> card_package){ //Créer un packet trié (à supprimer si inutile)
-        return;
-    }
-
-    public void mix_package(ArrayList<Cards> card_package){ // "mélange" le packet (croupier avantageux)
-        return;
     }
 
     public String getSign() {
--- Projet-Casino/src/games/Cards_Package.java
+++ Projet-Casino/src/games/Cards_Package.java
@@ -1,6 +1,8 @@
 package games;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Cards_Package {
     private ArrayList<Cards> card_package;
@@ -16,11 +18,24 @@ public class Cards_Package {
         return false;
     }
 
-    public void sorted_package(ArrayList<Cards> card_package){ //Créer un packet trié (à supprimer si inutile)
+    public void create_package(ArrayList<Cards> card_package){ //Créer un packet trié (à supprimer si inutile)
+        if(empty_package(card_package)){
+            for(int i =0; i<12; i++){
+                Cards heart = new Cards(i, "HEART");
+                card_package.add(heart);
+                Cards tile = new Cards(i, "TILE");
+                card_package.add(tile);
+                Cards spade = new Cards(i, "SPADE");
+                card_package.add(spade);
+                Cards clover = new Cards(i, "CLOVER");
+                card_package.add(clover);
+            }
+        }
+
 
     }
 
-    public void mix_package(ArrayList<Cards> card_package){ // "mélange" le packet (croupier avantageux)
-        return;
+    public void mix_package(ArrayList<Cards> card_package){ // "mélange" le packet (croupier avantageux) A compléter
+        Collections.shuffle(card_package);
     }
 }
