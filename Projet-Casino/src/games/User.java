b1bb8b0fafdc56f32989ca3984c0791c9cbf8ee2
loicfery <loic-fery@orange.fr>
2021-04-23 15:57:42 +0200

    modification classe User

--- Projet-Casino/src/games/User.java
+++ Projet-Casino/src/games/User.java
@@ -2,19 +2,59 @@ package games;
 
 public class User {
 
-    private String nomU;
-    private String prenomU;
-    private String pseudoU;
-    private String adressmail;
+    private String pseudo;
+    private String email;
     private String password;
-    private int coins;
+    private int numberOfToken;
+    private int numberOfMoney;
+    private String rank;
 
-    public User(String nomU, String prenomU, String pseudoU, String adressmail, String password, int coins) {
-        this.nomU = nomU;
-        this.prenomU = prenomU;
-        this.pseudoU = pseudoU;
-        this.adressmail = adressmail;
+    public User(String pseudo, String email, String password,int numberOfMoney, int numberOfToken, String rank) {
+        this.pseudo = pseudo;
+        this.email = email;
         this.password = password;
-        this.coins = coins;
+        this.numberOfToken = numberOfToken;
+        this.rank = rank;
+        this.numberOfMoney = numberOfMoney;
+    }
+
+    public String getPseudo(){return pseudo;}
+    public String getEmail(){return email;}
+    public String getPassword(){return password;}
+    public int getNumberOfToken(){return numberOfToken;}
+    public int getNumberOfMoney(){return numberOfMoney;}
+    public String getRank(){return rank;}
+
+
+    /** Récupère le nombre total de jeton de l'utilisateur **/
+    public void getToken(){
+        //recupération nombre de token dans base de donnée
+        //this.numberOfToken = ...;
+    }
+
+    /** Récupère la somme totale d'argent de l'utilisateur **/
+    public void getMoney(){
+        //recupération somme argent dans base de donnée
+    }
+
+    /** Ajoute des jetons **/
+    public void addToken(int numberOfToken){
+        this.numberOfToken = this.numberOfToken + numberOfToken;
+    }
+
+    /** Ajoute une somme d'argent **/
+    public void addMoney(int numberOfMoney){
+        this.numberOfMoney = this.numberOfMoney + numberOfMoney;
+    }
+
+    /** Enlève des jetons **/
+    public void removeToken(int numberOfToken){
+        this.numberOfToken = this.numberOfToken - numberOfToken;
+    }
+
+    /** Enlève une somme d'argent **/
+    public void removeMoney(int numberOfMoney){
+        this.numberOfMoney = this.numberOfMoney - numberOfMoney;
     }
 }
+
