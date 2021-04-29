bd5facf1fc55ac22d921997c07efd1e9868a8b23
loicfery <loic-fery@orange.fr>
2021-04-29 16:08:13 +0200

    update interface roulette

--- Projet-Casino/src/sample/RouletteMenuController.java
+++ Projet-Casino/src/sample/RouletteMenuController.java
@@ -8,22 +8,32 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class RouletteMenuController {
 
-    private final int ORIGIN_X_TOKEN_CIRCLE = 336;
-    private final int ORIGIN_Y_TOKEN_CIRCLE = 262;
-    private final int ORIGIN_X_TOKEN_LABEL = 325;
-    private final int ORIGIN_Y_TOKEN_LABEL = 252;
+    private final int ORIGIN_X_1 = 320;
+    private final int ORIGIN_Y_1 = 78;
+    private final int END_X_1 = 1046;
+    private final int END_Y_1 = 222;
+    private final int ORIGIN_X_2 = 383;
+    private final int ORIGIN_Y_2 = 222;
+    private final int END_X_2 = 992;
+    private final int END_Y_2 = 318;
+
+    private final int RADIUS_TOKEN = 16;
+
+    private final int ORIGIN_X_TOKEN = 336;
+    private final int ORIGIN_Y_TOKEN = 262;
 
     private User user;
     private Stage stage;
 
     private List<Circle> listOfCircleToken = new ArrayList<>();
     private List<Label> listLabelToken = new ArrayList<>();
+    private List<Case> listOfCase = new ArrayList<>();
+    private int tokenUsed = 0;
 
      @FXML
      private Label labelProfit;
@@ -148,19 +158,134 @@ public class RouletteMenuController {
         listLabelToken.add(labelToken13);
         listLabelToken.add(labelToken14);
         listLabelToken.add(labelToken15);
+
+        setCasePosition();
     }
 
+    public void setCasePosition(){
+        listOfCase.add(new Case(320,78,384,223,"green","0"));
+        listOfCase.add(new Case(383,174,434,222,"red","1"));
+        listOfCase.add(new Case(383,126,434,174,"black","2"));
+        listOfCase.add(new Case(383,78,434,126,"red","3"));
+        listOfCase.add(new Case(434,174,485,222,"black","4"));
+        listOfCase.add(new Case(434,126,485,174,"red","5"));
+        listOfCase.add(new Case(434,78,485,126,"black","6"));
+        listOfCase.add(new Case(485,174,536,222,"red","7"));
+        listOfCase.add(new Case(485,126,536,174,"black","8"));
+        listOfCase.add(new Case(485,78,536,126,"red","9"));
+        listOfCase.add(new Case(536,174,587,222,"black","10"));
+        listOfCase.add(new Case(536,126,587,174,"black","11"));
+        listOfCase.add(new Case(536,78,587,126,"red","12"));
+        listOfCase.add(new Case(587,174,638,222,"black","13"));
+        listOfCase.add(new Case(587,126,638,174,"red","14"));
+        listOfCase.add(new Case(587,78,638,126,"black","15"));
+        listOfCase.add(new Case(638,174,689,222,"red","16"));
+        listOfCase.add(new Case(638,126,689,174,"black","17"));
+        listOfCase.add(new Case(638,78,689,126,"red","18"));
+        listOfCase.add(new Case(689,174,740,222,"red","19"));
+        listOfCase.add(new Case(689,126,740,174,"black","20"));
+        listOfCase.add(new Case(689,78,740,126,"red","21"));
+        listOfCase.add(new Case(740,78,791,126,"black","22"));
+        listOfCase.add(new Case(740,126,791,174,"red","23"));
+        listOfCase.add(new Case(740,78,791,126,"black","24"));
+        listOfCase.add(new Case(791,78,842,126,"red","25"));
+        listOfCase.add(new Case(791,126,842,174,"black","26"));
+        listOfCase.add(new Case(791,78,842,126,"red","27"));
+        listOfCase.add(new Case(842,78,893,126,"black","28"));
+        listOfCase.add(new Case(842,78,893,126,"black","29"));
+        listOfCase.add(new Case(842,78,893,126,"red","30"));
+        listOfCase.add(new Case(893,78,944,126,"black","31"));
+        listOfCase.add(new Case(893,78,944,126,"red","32"));
+        listOfCase.add(new Case(893,78,944,126,"black","33"));
+        listOfCase.add(new Case(944,78,995,126,"red","34"));
+        listOfCase.add(new Case(944,78,995,126,"black","35"));
+        listOfCase.add(new Case(944,78,995,126,"red","36"));
+        listOfCase.add(new Case(995,78,1046,126,"green","1st"));
+        listOfCase.add(new Case(995,78,1046,126,"green","2nd"));
+        listOfCase.add(new Case(995,78,1046,126,"green","3rd"));
+        listOfCase.add(new Case(383,222,587,270,"green","1-12"));
+        listOfCase.add(new Case(383,270,483,318,"green","1-18"));
+        listOfCase.add(new Case(484,270,584,318,"green","even"));
+        listOfCase.add(new Case(587,222,791,270,"green","13-24"));
+        listOfCase.add(new Case(587,270,687,718,"green","red"));
+        listOfCase.add(new Case(688,270,788,718,"green","black"));
+        listOfCase.add(new Case(791,222,995,270,"green","25-36"));
+        listOfCase.add(new Case(791,270,891,318,"green","odd"));
+        listOfCase.add(new Case(892,270,992,318,"green","19-36"));
+    }
 
-    public void betToken(MouseEvent mouseEvent) {
-        int mousePositionX = 0;
-        int mousePositionY = 0;
+    public void choosePositionToken(MouseEvent mouseEvent) {
+        int mousePositionX = (int)(mouseEvent.getX());
+        int mousePositionY = (int)(mouseEvent.getY());
 
-        if(mouseEvent.getButton() == MouseButton.PRIMARY){
-            mousePositionX = (int)(mouseEvent.getX());
-            mousePositionY = (int)(mouseEvent.getY() * 10);
-            listOfCircleToken.get(0).setLayoutX(mousePositionX);
-            listOfCircleToken.get(0).setLayoutY(mousePositionY);
-            System.out.println(mousePositionX+", "+mousePositionY);
+        if(mouseEvent.getButton() == MouseButton.PRIMARY){ //clique gauche
+            if(isPositionMouseGood(mousePositionX,mousePositionY)){
+                setPositionToken(mousePositionX,mousePositionY,listOfCircleToken.get(tokenUsed),listLabelToken.get(tokenUsed),true);
+                betToken(mousePositionX,mousePositionY);
+            }
+        }
+        if(mouseEvent.getButton() == MouseButton.SECONDARY){ //clique droit
+            // menu modification token
+            if(tokenUsed > 0) {
+                tokenUsed--;
+            }
+            setPositionToken(ORIGIN_X_TOKEN,ORIGIN_Y_TOKEN,listOfCircleToken.get(tokenUsed),listLabelToken.get(tokenUsed),false);
+        }
+    }
+
+    public void setPositionToken(int positionX, int positionY, Circle circleToken, Label labelToken, boolean state){
+        circleToken.setLayoutX(positionX);
+        circleToken.setLayoutY(positionY);
+        circleToken.setVisible(state);
+        labelToken.setLayoutX(positionX - 10);
+        labelToken.setLayoutY(positionY - 10);
+        labelToken.setVisible(state);
+    }
+
+    public boolean isPositionMouseGood(int positionX, int positionY){
+        if((positionX >= ORIGIN_X_1 && positionX <= END_X_1) && (positionY >= ORIGIN_Y_1 && positionY <= END_Y_1)){
+            return true;
+        }
+        else{
+            if((positionX >= ORIGIN_X_2 && positionX <= END_X_2) && (positionY >= ORIGIN_Y_2 && positionY <= END_Y_2)){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    public void betToken(int positionXToken, int positionYToken){
+        for(int index = 0; index < listOfCase.size(); index ++){
+            if(tokenInTheCase(listOfCase.get(index),positionXToken,positionYToken)){
+                //recup case avec token dessus
+            }
+        }
+        tokenUsed++;
+    }
+
+    public boolean tokenInTheCase(Case cases, int positionXToken, int positionYToken){
+        if(((positionXToken + RADIUS_TOKEN) >= cases.getOriginX() && (positionXToken + RADIUS_TOKEN) <= cases.getEndX()) && (positionYToken >= cases.getOriginY() && positionYToken <= cases.getEndY())){
+            return true;
+        }
+        else {
+            if(((positionXToken - RADIUS_TOKEN) >= cases.getOriginX() && (positionXToken - RADIUS_TOKEN) <= cases.getEndX()) && (positionYToken >= cases.getOriginY() && positionYToken <= cases.getEndY())){
+                return true;
+            }
+            else {
+                if(((positionYToken + RADIUS_TOKEN) >= cases.getOriginY() && (positionYToken + RADIUS_TOKEN) <= cases.getEndY()) && (positionXToken >= cases.getOriginX() && positionXToken <= cases.getEndX())){
+                    return true;
+                }
+                else{
+                    if(((positionYToken - RADIUS_TOKEN) >= cases.getOriginY() && (positionYToken - RADIUS_TOKEN) <= cases.getEndY()) && (positionXToken >= cases.getOriginX() && positionXToken <= cases.getOriginX())){
+                        return true;
+                    }
+                    else{
+                        return false;
+                    }
+                }
+            }
         }
     }
 }
