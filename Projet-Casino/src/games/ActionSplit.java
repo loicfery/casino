d865637ccdbbf712963ceac45cc8adead3f8b754
MaxLGD89 <maximelegal89100@gmail.com>
2021-04-29 11:32:35 +0200

    BlackJack

--- Projet-Casino/src/games/ActionSplit.java
+++ Projet-Casino/src/games/ActionSplit.java
@@ -2,7 +2,7 @@ package games;
 
 public class ActionSplit implements ActionBlackJack {
     @Override
-    public void action(UserHand userHand, Cards_Package cards_package, Bet bet) {
+    public void action(UserHand userHand, Cards_Package cards_package, Bet bet, User user) {
 
     }
 }
--- Projet-Casino/src/games/ActionSurrender.java
+++ Projet-Casino/src/games/ActionSurrender.java
@@ -2,7 +2,8 @@ package games;
 
 public class ActionSurrender implements ActionBlackJack {
     @Override
-    public void action(UserHand userHand, Cards_Package cards_package, Bet bet) {
-        bet.setValueOfBetTotal(bet.getValueOfBetTotal()/2);
+    public void action(UserHand userHand, Cards_Package cards_package, Bet bet, User user) {
+        int surrender  = bet.getBet(user);
+        bet.addBet(surrender, user);
     }
 }
