9eaa0d6627583146b85d2f00bd8786ea3729078a
loicfery <loic-fery@orange.fr>
2021-05-14 16:04:56 +0200

    update database

---
+++ Projet-Casino/src/sample/DatabaseName.java
@@ -0,0 +1,105 @@
+package sample;
+
+public class DatabaseName {
+    //show tables from casino; pour recup les noms des tables (pas sûre de l'utiliser)
+
+    private final String tableUser = "";
+    private final String tableUserColumnMailUser = "";
+    private final String tableUserColumnPseudo = "";
+    private final String tableUserColumnToken = "";
+    private final String tableUserColumnMoney = "";
+
+    private final String tableHistoryExchangeToken = "";
+    private final String tableHistoryExchangeTokenColumnMailUser = "";
+    private final String tableHistoryExchangeTokenColumnPriceToken = "";
+    private final String tableHistoryExchangeTokenColumnTokenGain = "";
+
+    private final String tableHistoryExchangeMoney = "";
+    private final String tableHistoryExchangeMoneyColumnMailUser = "";
+    private final String tableHistoryExchangeMoneyColumnPriceMoney = "";
+    private final String tableHistoryExchangeMoneyColumnMoneyGain = "";
+
+    private final String tableHistoryPartyGamed = "";
+    private final String tableHistoryPartyGamedColumnMailUser = "";
+    private final String tableHistoryPartyGamedColumnNameGame = "";
+    private final String tableHistoryPartyGamedColumnTokenGain = "";
+
+    private final String tableGame = "";
+    private final String tableGameColumnNameGame = "";
+
+    public String getTableUser() {
+        return tableUser;
+    }
+
+    public String getTableUserColumnMailUser() {
+        return tableUserColumnMailUser;
+    }
+
+    public String getTableUserColumnPseudo() {
+        return tableUserColumnPseudo;
+    }
+
+    public String getTableUserColumnToken() {
+        return tableUserColumnToken;
+    }
+
+    public String getTableUserColumnMoney() {
+        return tableUserColumnMoney;
+    }
+
+    public String getTableHistoryExchangeToken() {
+        return tableHistoryExchangeToken;
+    }
+
+    public String getTableHistoryExchangeTokenColumnMailUser() {
+        return tableHistoryExchangeTokenColumnMailUser;
+    }
+
+    public String getTableHistoryExchangeTokenColumnPriceToken() {
+        return tableHistoryExchangeTokenColumnPriceToken;
+    }
+
+    public String getTableHistoryExchangeTokenColumnTokenGain() {
+        return tableHistoryExchangeTokenColumnTokenGain;
+    }
+
+    public String getTableHistoryExchangeMoney() {
+        return tableHistoryExchangeMoney;
+    }
+
+    public String getTableHistoryExchangeMoneyColumnMailUser() {
+        return tableHistoryExchangeMoneyColumnMailUser;
+    }
+
+    public String getTableHistoryExchangeMoneyColumnPriceMoney() {
+        return tableHistoryExchangeMoneyColumnPriceMoney;
+    }
+
+    public String getTableHistoryExchangeMoneyColumnMoneyGain() {
+        return tableHistoryExchangeMoneyColumnMoneyGain;
+    }
+
+    public String getTableHistoryPartyGamed() {
+        return tableHistoryPartyGamed;
+    }
+
+    public String getTableHistoryPartyGamedColumnMailUser() {
+        return tableHistoryPartyGamedColumnMailUser;
+    }
+
+    public String getTableHistoryPartyGamedColumnNameGame() {
+        return tableHistoryPartyGamedColumnNameGame;
+    }
+
+    public String getTableHistoryPartyGamedColumnTokenGain() {
+        return tableHistoryPartyGamedColumnTokenGain;
+    }
+
+    public String getTableGame() {
+        return tableGame;
+    }
+
+    public String getTableGameColumnNameGame() {
+        return tableGameColumnNameGame;
+    }
+}
