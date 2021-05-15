62ada3dbf866f313c12b24a9b1891bd46e99528c
loicfery <loic-fery@orange.fr>
2021-05-15 11:35:22 +0200

    update

--- Projet-Casino/src/sample/Main.java
+++ Projet-Casino/src/sample/Main.java
@@ -11,9 +11,9 @@ public class Main extends Application {
     }
 
     public void start(Stage stage) throws  Exception{
-        String bd = "projet";
+        String bd = "casino";
         String userName = "root";
-        String password = "Poussin13!";
+        String password = "";
         Database database = new Database(bd,userName,password); //si vous voulez tester avec la votre changer les valeurs
         try {
             database.importSqlFile("bdcasino.sql");
