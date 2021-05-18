842f5dd792e1f43cf5b0bbd1fe064d64c119c029
Houssem SAID <houssem-eddine.said@etu.univ-amu.fr>
2021-05-18 19:07:06 +0200

    Changement methode roulette

--- Projet-Casino/src/games/Roulette.java
+++ Projet-Casino/src/games/Roulette.java
@@ -20,25 +20,27 @@ public class Roulette {
         return number;
     }
 
-    public int Gain(List<BetRoulette> listOfBet, int number){
-        for(int i=0;i<listOfBet.size();i++){
-            for(int j=0;j<listOfBet.get(i).cells.size();j++) {
-                if(listOfBet.get(i).cells.get(j).number==number)
-                    return listOfBet.get(i).valueOfMultiply(listOfBet.get(i).chooseMultiply(listOfBet.get(i).cells));
-            }
+    public int Gain(List<Cell> listOfCells, int number){
+        for(int j=0;j<listOfCells.size();j++) {
+            if(listOfCells.get(j).getNumber()==number)
+                return listOfBet.get(j).valueOfMultiply(listOfBet.get(j).chooseMultiply(listOfCells))*listOfBet.get(j).valueOfBet;
         }
         return 0;
     }
 
-    /*public int FinalGain(){
-    }*/
+    public int FinalGain(int number){
+        int finalGain=0;
+        for(int i=0;i<listOfBet.size();i++){
+            for(int j=0;j<listOfBet.get(i).cells.size();j++){
+                finalGain+=Gain(listOfBet.get(i).cells,number);
+            }
+        }
+        return finalGain;
+    }
 
     public void ResetBetList(){
         listOfBet.clear();
     }
-
-    /** une méthode qui prend en paramètre une liste de case d'une mise, et la case choisit aléatoireemnt
-     * et retourne le gain de cette mise (0 si aucun gain)  **/
 
     /** une méthode qui calcule tous les gains et retourne le gain final **/
 
